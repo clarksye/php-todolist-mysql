e7f0326c613cfb8283458aded98a12a0334a715f
clarksye <burhanbasyri@gmail.com>
2021-06-08 11:01:35 -0700

    Add database class

--- Config/Database.php
+++ Config/Database.php
@@ -2,4 +2,13 @@
 
 class Database {
     
+    static function getConnection(): \PDO
+    {
+        $host = "localhost";
+        $database = "php_todolist_mysql";
+        $username = "root";
+        $password = "";
+
+        return new \PDO("mysql:host=$host;dbnames=$database", $username, $password);
+    }
 }
