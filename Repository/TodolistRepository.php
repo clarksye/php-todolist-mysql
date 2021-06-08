91b5e524abdf7ae50cb914302c9b99972dfbd13a
clarksye <burhanbasyri@gmail.com>
2021-06-08 11:32:33 -0700

    add save, remove method from database

--- Repository/TodolistRepository.php
+++ Repository/TodolistRepository.php
@@ -16,26 +16,50 @@ class TodolistRepositoryImpl implements TodolistRepository {
 
     public array $todolist = array();
 
+    private \PDO $connection;
+
+    public function __construct(\PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
     function save(Todolist $todolist): void
     {
-        $number = sizeof($this->todolist) + 1;
-        $this->todolist[$number] = $todolist;
+        // $number = sizeof($this->todolist) + 1;
+        // $this->todolist[$number] = $todolist;
+
+        $sql = "INSERT INTO todolist(todo) VALUES (?)";
+        $statement = $this->connection->prepare($sql);
+        $statement->execute([$todolist->getTodo()]);
+
     }
 
     function remove(int $number): bool
     {
 
-        if ($number > sizeof($this->todolist)) {
+        // if ($number > sizeof($this->todolist)) {
+        //     return false;
+        // }
+
+        // for ($i = $number; $i < sizeof($this->todolist); $i++) {
+        //     $this->todolist[$i] = $this->todolist[$i + 1];
+        // }
+
+        // unset($this->todolist[sizeof($this->todolist)]);
+
+        // return true;
+
+
+
+        $sql = "DELETE FROM todolist WHERE id = ?";
+        $statement = $this->connection->prepare($sql);
+        $statement->execute([$number]);
+
+        if($statement->rowCount() == 1 ) {
+            return true;
+        } else {
             return false;
         }
-
-        for ($i = $number; $i < sizeof($this->todolist); $i++) {
-            $this->todolist[$i] = $this->todolist[$i + 1];
-        }
-
-        unset($this->todolist[sizeof($this->todolist)]);
-
-        return true;
     }
 
     function findAll(): array
